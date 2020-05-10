b4014d72c20a22a3f02cf276db48998026e0f9a5
Patricia Santana Cruz <patriciasantanacruz@gmail.com>
2020-05-10 20:55:41 +0100

    Added bird obstacle

--- scripts/game.js
+++ scripts/game.js
@@ -18,26 +18,45 @@ var config = {
     render: { pixelArt: true }
 };
 
+var self;
 var game = new Phaser.Game(config);
 var dragonF;
 var trees;
-var bird;
 var cursor;
 var moreTrees = 0;
+var bird;
 
 function preload() {
     this.load.image('background', 'assets/images/background.png');
     this.load.spritesheet('dragonF_fly', 'assets/images/dragonF_flying.png', { frameWidth: 243, frameHeight: 195 });
     this.load.spritesheet('dragonF_explode', 'assets/images/dragonF_explode.png', { frameWidth: 378, frameHeight: 211 });
     this.load.image('tree', 'assets/images/tree2_psc.png');
+    this.load.atlas('bird', 'assets/images/bird_flying.png', 'assets/images/bird_flying.json');
 }
 
 function create() {
+
+    self = this;
+
     this.add.image(400, 250, 'background');
+
+    // bird
+    this.anims.create({
+        key: 'bird_fly',
+        frames: this.anims.generateFrameNames('bird', {
+            //start: 1,
+            end: 10,
+            zeroPad: 4,
+            prefix: 'Blue_Bird_Flying',
+            suffix: '.png'
+        }),
+        frameRate: 60,
+        repeat: -1
+    });
 
     // dragonFly
     dragonF = this.physics.add.sprite(100, 100, 'dragonF_fly');
-    dragonF.setScale(0.4);
+    dragonF.setScale(0.30);
     dragonF.setBounce(0.4);
     dragonF.setCollideWorldBounds(true);
     dragonF.body.onWorldBounds = true;
@@ -60,18 +79,18 @@ function create() {
     // Keys control
     cursor = this.input.keyboard.createCursorKeys();
 
+    // Obstacles:
     // trees
     trees = this.physics.add.group();
-    addTree(620,350);
+    addObstacles(620,350);
 
     // Collision control
     this.physics.add.collider(dragonF, trees, hitObstacle, null, this);
 }
 
-function addTree(x, y) {
+function addObstacles(x, y) {
     var tree = trees.create(x, y, 'tree');
     var treeType = Math.floor(Math.random() * 5);
-    console.log(treeType);
     switch (treeType) {
         case 0:
             tree.scaleX = .4;
@@ -87,8 +106,11 @@ function addTree(x, y) {
         case 3:
             tree.y += 170;
             tree.scaleX = .7;
+            addBird(tree);
+            break;
         case 4:
             tree.y += 60;
+            break;
         default:
             break;
     }
@@ -99,19 +121,34 @@ function addTree(x, y) {
     return tree;
 }
 
+function addBird(tree){
+    bird = self.physics.add.sprite(tree.x + 200, tree.y - 200, 'bird');
+    bird.body.allowGravity = false;
+    bird.setScale(0.25);
+    bird.setVelocityX(-200);
+    bird.body.immovable = true;
+    bird.checkWorldBounds = true;
+    bird.outOfBoundsKill = true;
+    bird.play('bird_fly');
+    self.physics.add.collider(dragonF, bird, hitObstacle, null, self);
+}
+
 function hitObstacle(actor, obstacle) {
     dragonF.play('dragonF_explode');
     dragonF.setTint(0xff0000);
     this.cameras.main.shake(500);
     this.physics.pause();
     trees.clear();
+    bird.anims.stop();
+    //this.scene.restart();
+    //this.anims.pauseAll();
 }
 
 function update() {
     // trees
     moreTrees++
     if (moreTrees === 100) {
-        var tree = addTree(950, 350);
+        var tree = addObstacles(950, 350);
         tree.setOrigin(0, 0);
         trees.setVelocityX(-200);
         moreTrees = 0;
