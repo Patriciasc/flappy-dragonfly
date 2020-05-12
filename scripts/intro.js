67bd0522a312d622b94800f25a816a99ba77cebd
Patricia Santana Cruz <patriciasantanacruz@gmail.com>
2020-05-12 22:35:23 +0100

    Connected scenes with basic functionality

--- scripts/intro.js
+++ scripts/intro.js
@@ -2,4 +2,12 @@ class intro extends Phaser.Scene {
     constructor() {
         super("introS");
     }
+
+    create() {
+        var text = this.add.text(20, 20, "Loading game...");
+        this.scene.start("level1S");
+
+        //var keyObj = this.scene.input.keyboard.addKey('ENTER');  // Get key object
+        //keyObj.on('down', function (event) { this.scene.start("level1S");});
+    }
 }
--- scripts/rating.js
+++ scripts/rating.js
@@ -2,4 +2,9 @@ class rating extends Phaser.Scene {
     constructor() {
         super("ratingS");
     }
+
+    create() {
+        var text = this.add.text(20, 20, "Loading raiting...");
+        //load next level
+    }
 }
